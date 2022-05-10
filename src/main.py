1fdb596f280c9fad46780ddc35937ecdfbaf16f0
Prajwal T R <prajwaltr93@gmail.com>
2022-05-10 19:59:07 +0530

    fix index issue

--- src/main.py
+++ src/main.py
@@ -139,7 +139,8 @@ def main():
         with open(path.join(output_dir_path, l_s['BookmarkTitle'] + ".pdf"), "wb") as out_file:
             out_writer = PdfFileWriter()
             for page_number in range(l_s['BookmarkPageNumber'], l_s['BookmarkLastPageNumber']):
-                page_got = input_file.pages[page_number]
+                # 0 based indexing of pages, compared to real-word page numbers
+                page_got = input_file.pages[page_number - 1]
                 out_writer.addPage(page_got)
 
             out_writer.write(out_file)
