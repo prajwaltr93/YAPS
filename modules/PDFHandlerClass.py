efd7fe93e6eddc833c6412fdd64b05cdc5493031
Prajwal T R <prajwaltr93@gmail.com>
2022-07-23 11:39:00 +0530

    pdfhandler class

---
+++ modules/PDFHandlerClass.py
@@ -0,0 +1,39 @@
+from os import mkdir, path
+from PyPDF2 import PdfFileReader, PdfFileWriter
+
+class PDFHandler:
+    def __check_or_create_output_dir(self, output_dir):
+        if not output_dir:
+            output_dir = f"{self.input_file_path}_{self.level}"
+        if not path.exists(output_dir):
+            mkdir(output_dir)
+        
+        return output_dir
+    
+    def __create_input_pdf_obj(self, input_file_path):
+        return PdfFileReader(input_file_path)
+
+    def __init__(self, output_path, level_store, input_file, level):
+        self.level_store = level_store
+        self.input_file_path = input_file
+        self.input_file = self.__create_input_pdf_obj(input_file_path=input_file)
+        self.level = level
+        self.output_path = self.__check_or_create_output_dir(output_dir=output_path)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        for l_s in self.level_store:
+            title_prep = l_s['BookmarkTitle'].replace("/", " or ")
+            page_range = range(l_s['BookmarkPageNumber'], l_s['BookmarkLastPageNumber'] + 1)
+            yield title_prep, page_range
+    
+    def write_page_range_to_file(self, page_range):
+        out_writer = PdfFileWriter()
+        for page_number in page_range:
+            # 0 based indexing of pages, compared to real-word page numbers
+            page_got = self.input_file.pages[page_number - 1]
+            out_writer.addPage(page_got)
+    
+        return out_writer
